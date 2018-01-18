ea1fde6015d6a87b2e9d9014cfddfa7dbd12974d
Will Reister <33736973+BlackMajix@users.noreply.github.com>
2018-01-18 15:23:26 -0800

    Deleting initial comments

--- homework00/Greeter.java
+++ homework00/Greeter.java
@@ -13,6 +13,7 @@
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2018-01-18  Will Reister  Initial writing and release
+ *  @version 1.0.1  2018-01-18  Will Reister  Deleting initial comments
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 public class HelloWorld {
@@ -22,8 +23,3 @@ public class HelloWorld {
     }
 
 }
-
-// you will need to insert your code here
-//  to compile, open a terminal window and type "javac Greeter.java" then press "Enter"
-//  to execute your code, in the terminal window type "java Greeter" then press "Enter"
-// you can remove these line comments and replace them with your code
