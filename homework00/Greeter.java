4722fed499ec6fa803839f45ed7ff797cd6d6795
Will Reister <33736973+BlackMajix@users.noreply.github.com>
2018-01-18 15:22:02 -0800

    Initial writing of greeter.java

---
+++ homework00/Greeter.java
@@ -0,0 +1,29 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Greeter.java
+ *  Purpose       :  Says "Hello world"; first program; provides a header template for Java programs
+ *  Author        :  Will Reister
+ *  Date          :  2018-01-18
+ *  Description   :  See purpose
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision Histor
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2018-01-18  Will Reister  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
+
+// you will need to insert your code here
+//  to compile, open a terminal window and type "javac Greeter.java" then press "Enter"
+//  to execute your code, in the terminal window type "java Greeter" then press "Enter"
+// you can remove these line comments and replace them with your code
